2108ec3e995a83b8510b1a967ee1cccffe555b82
GTessarini <gtessarini@uol.com.br>
2017-09-21 16:40:42 -0300

    Update scriptNotificacaoAsana.js

--- scriptNotificacaoAsana.js
+++ scriptNotificacaoAsana.js
@@ -2,7 +2,7 @@ var intervalCheckNotification = window.setInterval(function(){
     try{
 	var hasNotificationsAsana = document.getElementsByClassName("has-newNotifications");
         if(typeof(hasNotificationsAsana) !== "undefined" && hasNotificationsAsana !== null && hasNotificationsAsana.length){
-            confirm("NOTIFICAÇÃO DE TASK ASANA");
+            confirm("ASANA TASK NOTIFICATION");
         }
     }catch(err){
         window.clearInterval(intervalCheckNotification);
@@ -11,6 +11,6 @@ var intervalCheckNotification = window.setInterval(function(){
 /* Gabriel Tessarini */
 
 /*
-var intervalCheckNotification=window.setInterval(function(){try{var e=document.getElementsByClassName("has-newNotifications");"undefined"!=typeof e&&null!==e&&e.length&&confirm("NOTIFICAÇÃO DE TASK ASANA")}catch(n){window.clearInterval(intervalCheckNotification)}},8e3);
+var intervalCheckNotification=window.setInterval(function(){try{var e=document.getElementsByClassName("has-newNotifications");"undefined"!=typeof e&&null!==e&&e.length&&confirm("ASANA TASK NOTIFICATION")}catch(n){window.clearInterval(intervalCheckNotification)}},8e3);
 // Gabriel Tessarini
 */
